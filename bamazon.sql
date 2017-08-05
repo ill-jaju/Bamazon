e821765a11bb2afb357d9ce8a9b5feae9b853206
jamie-jessi <jamiejudi@gmail.com>
2017-08-04 23:27:33 -0500

    populated table

--- bamazon.sql
+++ bamazon.sql
@@ -1,11 +1,44 @@
+drop database if exists bamazondb;
+
 create database bamazondb;
 
 use bamazondb;
 
 create table products (
-	item_id int not null auto_increment,
+    item_id int not null auto_increment,
     product_name varchar(100) NULL,
     department_name varchar(100) NUll,
     price decimal(10,2) NULL,
     stock_qty int NULL,
     primary key (item_id)
+);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('potion', 'recovery', 50.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('smoke bomb', 'support', 100.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('impaler', 'debuff', 500.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('molotov', 'attack', 400.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('vagyrisk claw', 'instant-death', 1000.00, 5);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('grenade', 'non-elemental', 80.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('gysahl greens', 'field', 100.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('power source', 'stat-upgrade', 1000.00, 99);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('buster sword', 'weapon', 1.00, 1);
+
+insert into products (product_name, department_name, price, stock_qty)
+values ('bangle', 'armor', 100.00, 1);
