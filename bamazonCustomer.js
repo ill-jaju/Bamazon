e2e4a3af94b020178dae56442d9527aff1e775aa
jamie-jessi <jamiejudi@gmail.com>
2017-08-05 00:58:04 -0500

    errors in display and functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,6 @@ connection.connect(function(err){
     if(err) throw err;
 
     itemDisplay();
-    userInquirer();
 });
 
 function itemDisplay() { //function to display available items
@@ -25,6 +24,8 @@ function itemDisplay() { //function to display available items
             console.log('-------------------------------------------------------');
         }
     });
+
+    userInquirer();
 
 }
 
@@ -43,7 +44,29 @@ function userInquirer() { //inquirer function for user input
         }
     ])
     .then(function(answer){
-        
-    })
+
+        var query = "SELECT * from products"
+        connection.query(query, function(err, res){
+
+            var j = answer.itemId - 1
+
+            console.log('you want to buy ' + answer.itemAmount + ' of id# ' + answer.itemId );
+
+            if (answer.itemAmount < res[j].stock_qty) {
+
+                var stockLeft = res[j].stock_qty - answer.itemAmount
+
+                "INSERT INTO bamazondb",
+                {
+                    stock_qty: stockLeft
+                }
+
+                console.log('there are ' + stockLeft + ' available' )
+            } else {
+                console.log('we no longer have that item');
+            }
+        });
+
+    });
 
 }
